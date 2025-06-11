db93a832ead2b6c5cb7750c93f1ad5153c492bda
김민수 <mkms1104@naver.com>
2025-06-11 18:56:23 +0900

    Business Layer 테스트 작성

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductType.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductType.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.domain.product;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 public enum ProductType {
     HANDMADE("제조 음료"),
@@ -10,4 +12,8 @@ public enum ProductType {
     ;
 
     private final String text;
+
+    public static boolean containsStockType(ProductType type) {
+        return List.of(BOTTLE, BAKERY).contains(type);
+    }
 }
---
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -0,0 +1,38 @@
+package sample.cafekiosk.spring.domain.product;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProductTypeTest {
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @Test
+    void containsStockType() {
+        // Given
+        ProductType givenType = ProductType.HANDMADE;
+
+        // When
+        boolean result = ProductType.containsStockType(givenType);
+
+        // Then
+        assertThat(result).isFalse();
+
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @Test
+    void containsStockType2() {
+        // Given
+        ProductType givenType = ProductType.BAKERY;
+
+        // When
+        boolean result = ProductType.containsStockType(givenType);
+
+        // Then
+        assertThat(result).isTrue();
+
+    }
+
+}
